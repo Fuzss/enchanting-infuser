560f4f80cb590f29ff5260dd50b294deb2f46388
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-03-22 22:33:00 +0100

    fix enchanting elytra

--- 1.21.11/Common/src/main/java/fuzs/enchantinginfuser/config/ModifiableItems.java
+++ 1.21.11/Common/src/main/java/fuzs/enchantinginfuser/config/ModifiableItems.java
@@ -1,6 +1,7 @@
 package fuzs.enchantinginfuser.config;
 
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 
 import java.util.function.Predicate;
 
@@ -15,7 +16,12 @@ public enum ModifiableItems {
         this.predicate = predicate;
     }
 
+    /**
+     * Do not use {@link ItemStack#isEnchantable()} as it requires
+     * {@link net.minecraft.core.component.DataComponents#ENCHANTABLE} to be present, which e.g., the elytra does not
+     * have.
+     */
     private static boolean isItemEnchantableOrEnchanted(ItemStack itemStack) {
-        return itemStack.isEnchantable() || itemStack.isEnchanted();
+        return EnchantmentHelper.canStoreEnchantments(itemStack) || itemStack.isEnchanted();
     }
 }
